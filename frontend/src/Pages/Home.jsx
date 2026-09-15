085abd42ff43daa493183c0872b2e572b479145f
Omar <omaryazliyev2004@gmail.com>
2026-09-15 11:49:14 +0500

    feat: replace table booking section image with full pizza photo and floating animation

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -419,7 +419,7 @@ export default function Home() {
                 </form>
               </div>
               <div className="s3-right">
-                <img src={wineGlassesImg} alt="Wine glasses" />
+                <img src={fod} alt="Pizza" />
               </div>
             </div>
             <img className="barg5" src={barg5} alt="" />
